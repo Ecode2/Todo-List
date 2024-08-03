d5d4fd398ea4d55b85eda391b65f401a0e322a10
Ecode2 <ecode5814@gmail.com>
2024-08-03 21:07:53 +0100

    changes

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Todo List
 
-This static todo list website is made wúsing javascript.
+This static todo list website is made using javascript.
 It stores all the tasks in your browsers localstorage 
 and you have the ability to mark a task as complete and to delete a task
 
--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js')
+            .then(registration => {
+                console.log("Service Worker registered with scope:", registration.scope);
+            }).catch(error => {
+                console.log("Service Worker registeration failed", error);
+            });
+    });
+}
+
 // Function to add task to json file on submit
 function addTask() {
     // Get task from input tag
